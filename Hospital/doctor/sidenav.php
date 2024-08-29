4c58001c5afcdf02385a77fa94f5e8ab474f09c5
Jay Bhagat <120237982+Jay1811004@users.noreply.github.com>
2024-08-29 16:01:45 +0530

    Add files via upload

---
+++ Hospital/doctor/sidenav.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<style>
+    .list-group {
+         background: linear-gradient(90deg, #17a2b8, #117a8b) !important;
+    }
+    .list-group-item {
+         background: linear-gradient(90deg, #17a2b8, #117a8b) !important;
+         padding: 20px;
+    }
+    
+</style>
+<body>
+<div class="list-group " style="height: 90vh;">
+    <a href="index.php" class="list-group-item list-group-item-action text-center text-white">Dashboard</a>
+    <a href="profile.php" class="list-group-item list-group-item-action text-center text-white">Profile</a>
+    <a href="admin.php" class="list-group-item list-group-item-action text-center text-white">Patient</a>
+    <a href="doctor.php" class="list-group-item list-group-item-action text-center text-white">Appointment</a>
+    <a href="" class="list-group-item list-group-item-action text-center text-white">Report</a>
+ </div>
+</body>
+</html>
